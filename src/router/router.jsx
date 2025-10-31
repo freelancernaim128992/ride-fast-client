9473bb2226da6dafb96770d42bdd17f35a2a2968
Arafat Islam Naim <freelancernaim128992@gmail.com>
2025-11-01 04:17:00 +0600

    maked layout, navbar and footer

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,7 +1,15 @@
 import { createBrowserRouter } from "react-router";
+import Home from "../pages/Home/Home/Home";
+import RootLayout from "../layout/RootLayout/RootLayout";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: RootLayout,
+    children: [
+        {
+            index: true,
+            Component: Home
+        }
+    ]
   },
 ]);
