344b385cc0efa57e75af8a5422283279de233c50
Arafat Islam Naim <freelancernaim128992@gmail.com>
2025-11-07 04:58:47 +0600

    making marchant section

---
+++ src/pages/Home/BeMarchant/BeMarchant.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import location from '../../../assets/location-merchant.png'
+
+const BeMarchant = () => {
+    return (
+        <div data-aos='flip-left' className="bg-[#03373D] max-w-5xl mx-auto mb-10 p-16 rounded-4xl bg-[url('assets/be-a-merchant-bg.png')] bg-no-repeat">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img
+                    src={location}
+                    className="max-w-sm rounded-lg"
+                />
+                <div>
+                    <h1 className="text-4xl font-bold text-white">Merchant and Customer Satisfaction is Our First Priority</h1>
+                    <p className="py-6 text-gray-200">
+                        We offer the lowest delivery charge with the highest value along with 100% safety of your product. Pathao courier delivers your parcels in every corner of Bangladesh right on time.
+                    </p>
+                    <button className="btn btn-primary text-black rounded-full">Become a Merchant</button>
+                    <button
+                        className="btn btn-outline ms-4 text-primary rounded-full shadow-md hover:shadow-lg hover:bg-transparent transition-shadow duration-300"
+                        onMouseEnter={(e) =>
+                            (e.currentTarget.style.boxShadow = "0 4px 10px #CAEB66")
+                        }
+                        onMouseLeave={(e) => (e.currentTarget.style.boxShadow = "none")}
+                    >
+                        Earn with Profast Courier
+                    </button>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default BeMarchant;
--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -3,6 +3,7 @@ import Banner from '../Banner/Banner';
 import Services from '../Services/Services';
 import ClientLogos from '../ClientLogos/ClientLogos';
 import KeyBenefits from '../KeyBenefits/KeyBenefits';
+import BeMarchant from '../BeMarchant/BeMarchant';
 
 const Home = () => {
     return (
@@ -11,6 +12,7 @@ const Home = () => {
             <Services></Services>
             <ClientLogos></ClientLogos>
             <KeyBenefits></KeyBenefits>
+            <BeMarchant></BeMarchant>
         </div>
     );
 };
